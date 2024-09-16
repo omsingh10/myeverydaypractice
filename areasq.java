71506e72e072dbd3b3f0e9fec66864b66e9ef1ae
Om singh(shiv) <114778480+omsingh10@users.noreply.github.com>
2024-09-16 19:54:03 +0530

    Add files via upload

---
+++ areasq.java
@@ -0,0 +1,16 @@
+package shivjava;
+import java.util.*;
+
+public class areasq {
+    public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            for(int i = 0 ; i <= 10 ; i++){
+
+                System.out.println( +n + " x " + i + " = " + n*i );
+            }
+
+
+        }
+}
